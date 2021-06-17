57c9e2687b22875b5c19622a196e1595851daa99
hotbagels <81516937+hotbagels@users.noreply.github.com>
2021-06-16 20:56:43 -0400

    fixed coonnection points between JS and HTML. planning on moving my copy of the HTML and re-cloning what is on main. just commiting what i have now JIC

--- assets/js/API-test-script.js
+++ assets/js/API-test-script.js
@@ -1,4 +1,4 @@
-var searchButton = document.querySelector('#art-search')
+var searchEl = document.querySelector('#searchEl')
 var artImage = document.querySelector('#art-image')
 var keywordSearch_QueryURL = 'https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&perPage=100&q='
 var objectSearch_QueryURL = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/'
@@ -14,7 +14,7 @@ var objIDs = [];
 
 function handleSearchSubmit(event) {
     event.preventDefault();
-    console.log("click");
+    // console.log("click");
     searchValue = document.querySelector('#search-input').value;
 
     fetch(keywordSearch_QueryURL + searchValue)
@@ -38,7 +38,7 @@ function handleSearchSubmit(event) {
                 j++;
 
                 objectC = data.objectIDs[j];
-                console.log("j after= " + j);
+                // console.log("j after= " + j);
             }
 
             // console.log(data.objectIDs.length);
@@ -89,6 +89,6 @@ function displayObjectData() {
 }
 
 
-searchButton.addEventListener('submit', handleSearchSubmit);
+searchEl.addEventListener('submit', handleSearchSubmit);
 
-console.log("pagebottom");
+// console.log("pagebottom");
