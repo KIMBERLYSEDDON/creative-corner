ecec5f950b63a933e8e95cdf383d85e1eae82003
hotbagels <81516937+hotbagels@users.noreply.github.com>
2021-06-16 20:08:55 -0400

    worked on met gallery image fetch

--- assets/js/API-test-script.js
+++ assets/js/API-test-script.js
@@ -1,13 +1,20 @@
 var searchButton = document.querySelector('#art-search')
 var artImage = document.querySelector('#art-image')
-var keywordSearch_QueryURL = 'https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&isHighlight=true&q='
+var keywordSearch_QueryURL = 'https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&perPage=100&q='
 var objectSearch_QueryURL = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/'
 var searchValue;
-var objectNum;
+
+var index = 0;
+
+var objectA;
+var objectB;
+var objectC;
+var data;
+var objIDs = [];
 
 function handleSearchSubmit(event) {
     event.preventDefault();
-
+    console.log("click");
     searchValue = document.querySelector('#search-input').value;
 
     fetch(keywordSearch_QueryURL + searchValue)
@@ -15,28 +22,68 @@ function handleSearchSubmit(event) {
             return response.json();
         })
         .then(function (data) {
+            // console.log(data.objectIDs.length);
             console.log(data);
-            objectNum = data.objectIDs[5];
-            console.log(data.objectIDs.length);
+            objIDs = data;
+
+
+            for (var index = 0; index <= 10; index++) {
+                var j = index
+                objectA = data.objectIDs[j];
+
+                j++;
+
+                objectB = data.objectIDs[j];
+
+                j++;
+
+                objectC = data.objectIDs[j];
+                console.log("j after= " + j);
+            }
+
+            // console.log(data.objectIDs.length);
             displayObjectData();
             return;
 
         });
 
 
+
 }
 
-function displayObjectData() {
-    console.log(objectNum);
 
-    fetch(objectSearch_QueryURL + objectNum)
+function displayObjectData() {
+
+
+
+    fetch(objectSearch_QueryURL + objectA)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
             console.log(data.primaryImage);
-            $('#art-image').html("<img src=" + data.primaryImage + ">");
+            $('#art-image').append("<img src=" + data.primaryImage + ">");
+        });
+
+    fetch(objectSearch_QueryURL + objectB)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            console.log(data.primaryImage);
+            $('#art-image').append("<img src=" + data.primaryImage + ">");
+        });
+
+    fetch(objectSearch_QueryURL + objectC)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            console.log(data.primaryImage);
+            $('#art-image').append("<img src=" + data.primaryImageSmall + ">");
         });
 
 }
@@ -44,3 +91,4 @@ function displayObjectData() {
 
 searchButton.addEventListener('submit', handleSearchSubmit);
 
+console.log("pagebottom");
