4f60f0698272a6e4bd2d770d538413761f5cd727
KIMBERLYSEDDON <82526676+KIMBERLYSEDDON@users.noreply.github.com>
2021-06-18 12:17:29 -0400

    worked on title/cloud positioning and animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,13 +15,35 @@ var getQuote = function (quote) {
       .catch(function (error) {
         alert('Unable to connect');
       });
-  };
+};
 
-  var displayQuote = function (quote, author){
+var displayQuote = function (quote, author){
       var quoteEl = document.createElement('p')
+      quoteEl.setAttribute("class", " uk-overlay uk-responsive-width uk-text-center uk-margin-xlarge-top")
+      // var quoteTxt = document.createElement('p')
+      // quoteTxt.setAttribute("class", "uk-text-center")
       quoteEl.textContent = '"' + quote + '"' + '   -' + author
-      console.log(quoteEl)
-      document.querySelector("#quote").appendChild(quoteEl)
+      // console.log(quoteTxt)
+      // quoteEl.appendChild(quoteTxt)
+      document.querySelector("#quote").append(quoteEl)
 
   }
 getQuote()
+
+
+// var displayQuote = function (quote, author){
+//   var quoteCloud = document.createElement('img')
+//   quoteCloud.setAttribute("src", "./images/cloudscreenshot.jpg")
+//   quoteCloud.setAttribute("alt", "thought cloud")
+//   // <img src="./images/cloudscreenshot.jpg" alt="cloud">
+//     var quoteEl = document.createElement('div')
+//     quoteEl.setAttribute("class", "uk-position-cover, border: 2px solid black")
+//     // var quoteTxt = document.createElement('p')
+//     quoteEl.setAttribute("class", "color: black, z-index: 5")
+//     quoteEl.textContent = '"' + quote + '"' + '   -' + author
+//     // console.log(quoteTxt)
+//     // quoteEl.appendChild(quoteTxt)
+//     quoteCloud.appendChild(quoteEl)
+//     document.querySelector("#quote").appendChild(quoteCloud)
+
+// }
